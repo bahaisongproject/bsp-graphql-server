b0271dca242507a2ba94be705d05031c25989fed
Dayyan Smith <dayyan.smith@gmail.com>
2020-04-13 23:05:06 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient()
+async function main() {
+    const allSongs = await prisma.song.findMany()
+    console.log(allSongs)
+}
+main()
+  .catch(e => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.disconnect()
+  })

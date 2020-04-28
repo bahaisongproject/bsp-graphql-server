aeb82771f2729ca366ddf378aa81413aea381fdf
Dayyan Smith <dayyan.smith@gmail.com>
2020-04-28 15:52:23 +0200

    Remove static file server

--- src/index.js
+++ src/index.js
@@ -7,25 +7,14 @@ const express = require('express');
 const app = express();
 const prisma = new PrismaClient();
 
-// new GraphQLServer({
-//   schema: makeSchema({
-//     types,
-//     plugins: [nexusPrismaPlugin()],
-//     outputs: {
-//       schema: __dirname + "/../schema.graphql",
-//       typegen: __dirname + "/generated/nexus.ts",
-//     },
-//   }),
-//   context: { prisma },
-// }).start(() => console.log(`🚀 GraphQL server ready at: http://localhost:4000\n`));
-
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.send('An alligator approaches!');
-});
-app.get('/.well-known/acme-challenge/XKFh8ZO8ntJHAqCE_ZmE9g6TcgnDcohS5gQr5oW3ROY', (req, res) => {
-  res.send('XKFh8ZO8ntJHAqCE_ZmE9g6TcgnDcohS5gQr5oW3ROY.GXkDLsnZLko_IqiZsi-SgsqhpMCaP3ivQUhJ5XzmeUU');
-});
-let server = app.listen(() => {
-  console.log('Listening', server.address().port)
-})
+new GraphQLServer({
+  schema: makeSchema({
+    types,
+    plugins: [nexusPrismaPlugin()],
+    outputs: {
+      schema: __dirname + "/../schema.graphql",
+      typegen: __dirname + "/generated/nexus.ts",
+    },
+  }),
+  context: { prisma },
+}).start(() => console.log(`🚀 GraphQL server ready at: http://localhost:4000\n`));
